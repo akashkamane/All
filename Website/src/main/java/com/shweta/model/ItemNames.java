e1c9650f59bce459813e9afb48ceccb5aa52f3fc
AKASH KAMANE <akashkamane97@gmail.com>
2019-08-19 01:41:46 +0530

    mvc

--- Website/src/main/java/com/shweta/model/ItemNames.java
+++ Website/src/main/java/com/shweta/model/ItemNames.java
@@ -1,11 +1,5 @@
 package com.shweta.model;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Vector;
-
-import javax.servlet.http.HttpSession;
-
 public class ItemNames {
 	
 }
